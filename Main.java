0cdb70db34e48589cf269695b06308e6eea1e859
navid <nnd1380@gmail.com>
2021-03-11 11:46:15 +0330

    commit sec

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
 public class Main{
     public static void main(String [] args){
         System.out.println("Hello git");
-<<<<<<< HEAD
         System.out.println("My name is: navid");
-        System.out.println("My student ID is <your id>");
-=======
-        System.out.println("My name is: <your name>");
         System.out.println("My student ID is 99243083");
->>>>>>> student-id
+
     }
 }
